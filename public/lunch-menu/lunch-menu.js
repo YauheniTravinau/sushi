b0dc656e51994ebf480136c4ccbf432520016d05
YauheniTravinau <j.travinov@gmail.com>
2024-09-09 16:38:23 +0200

    sutr11

--- public/lunch-menu/lunch-menu.js
+++
@@ -1,6 +0,0 @@
-function changeImage(mainImgId, clickedThumb) {
-    let mainImage = document.getElementById(mainImgId);
-    let mainImageSrc = mainImage.src;
-    mainImage.src = clickedThumb.src;
-    clickedThumb.src = mainImageSrc;
-}
