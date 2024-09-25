4972a5421f4121c23f2ec0cf67e04042448133fe
YauheniTravinau <j.travinov@gmail.com>
2024-09-25 21:29:34 +0200

    sutr21

--- public/menu2/mini/mini.js
+++
@@ -1,9 +0,0 @@
-/*мини меню*/
-document.addEventListener('DOMContentLoaded', () => {
-    const menuWrapper = document.querySelector('.mini-menu-wrapper');
-    const menuToggle = document.querySelector('.mini-menu-toggle');
-
-    menuToggle.addEventListener('click', () => {
-        menuWrapper.classList.toggle('active');
-    });
-});
