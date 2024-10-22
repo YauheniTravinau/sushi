af36ff3f9bda5fea2309ee19b77ecb793c171251
YauheniTravinau <j.travinov@gmail.com>
2024-10-22 16:49:11 +0200

    sutr23

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
     const enterButton = document.getElementById("enter-button");
+    const video = document.querySelector('.background-video');
 
     // Функция для перехода по ссылке
     function redirectToMainPage() {
         window.location.href = enterButton.href; // Переход по ссылке кнопки
     }
 
-    // Установка таймера на 20 секунд
+    // Установка таймера на 20 секунд для автоматического перехода
     setTimeout(function() {
         redirectToMainPage(); // Вызов функции для автоматического перехода
     }, 20000); // 20 секунд (20000 миллисекунд)
+
+    // Проверка состояния готовности видео и перенаправление через 5 секунд
+    setTimeout(function() {
+        if (video.readyState < 3) { // Если видео не готово к воспроизведению
+            window.location.href = "/public/glowna/glowna.html"; // Переход на главную
+        }
+    }, 5000); // 5 секунд на загрузку видео
 });
